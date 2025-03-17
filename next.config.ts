4ee674275bfcea1f5ff8d48c9f09378e47285745
Iyola Oyabiyi O <iyolaoyabiyi@gmail.com>
2025-03-17 04:00:51 +0100

    Configure Next.js for static export and set base path; enable unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/iyoolaoyabiyi',
 };
 
 export default nextConfig;
